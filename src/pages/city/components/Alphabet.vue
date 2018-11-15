4d139640d37114401365a28963790725f7895989
chenglin <837460387@qq.com>
2018-11-15 21:35:42 +0800

    initialize

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="list">
+    <div
+      class="item"
+      v-for="item of letters"
+      :key="item"
+      @touchstart.prevent="handleTouchStart"
+      @touchmove="handleTouchMove"
+      @touchend="handleTouchEnd"
+      @click="handleLetterChange(item)"
+      :ref="item"
+    >{{item}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      touchStatus: false,
+      startY: 0
+    }
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop
+  },
+  methods: {
+    handleLetterChange: function (letter) {
+      this.$emit('change', letter)
+    },
+    handleTouchStart: function () {
+      this.touchStatus = true
+    },
+    handleTouchMove: function (e) {
+      if (this.touchStatus) {
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+        this.timer = setTimeout(() => {
+          const touchY = e.touches[0].clientY - 79
+          const index = Math.floor((touchY - this.startY) / 20)
+          if (index >= 0 && index < this.letters.length) {
+            this.$emit('change', this.letters[index])
+          }
+        }, 16)
+      }
+    },
+    handleTouchEnd: function () {
+      this.touchStatus = false
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/variable.styl'
+  .list
+    display: flex
+    flex-direction: column
+    justify-content: center
+    position: absolute
+    top: 1.58rem
+    right: 0
+    bottom: 0
+    width: .4rem
+    .item
+      line-height: .4rem
+      color: $bgColor
+      text-align: center
+</style>
